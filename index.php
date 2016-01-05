7443fd6d9a9b38085c30f0c733a603d6f8a4a0ba
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-01-04 17:03:33 -0700

    add home page content

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-		<!-- Google Fonts -->
+		<!-- Google Fonts: Didact Gothic -->
+		<link href='//fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
 
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
@@ -24,7 +25,7 @@
 		<![endif]-->
 
 		<!-- jQuery (required for Bootstap's JS plugins) -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" type="text/javascript"></script>
 
 		<!-- Bootstrap latest complied and minified JavaScript -->
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
@@ -40,14 +41,150 @@
 	<body class="sfooter">
 		<div class="sfooter-content">
 
-			<header>
-
+			<header id="top">
+				<nav class="navbar navbar-fixed-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapsed">
+								<span class="sr-only">Main Menu</span>
+								<i class="fa fa-th fa-2x"></i>
+							</button>
+							<a class="navbar-brand" href="#">Deep Dive Dylan</a>
+						</div>
+						<div class="collapse navbar-collapse" id="navbar-collapsed">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#top"><i class="fa fa-home"></i></a></li>
+								<li><a href="#about">About</a></li>
+								<li><a href="#">Resume</a></li>
+								<li><a role="button" data-toggle="modal" data-target="#contactModal">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
 			</header>
 
-		</div>
+			<main>
+				<section class="parallax" data-stellar-background-ratio="0.5" id="welcome">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="jumbotron">
+									<h2>Dylan McDonald</h2>
+									<p class="small">Computer scientist, teacher, free thinker, lover of green chile, & the universe around me.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!--teaching-->
+				<section class="parallax" data-stellar-background-ratio="0.5" id="welcome">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12">
+								<h2>Teaching</h2>
+								<p>I have been teaching in some capacity since 2004. Computer science has been my craft all my life. A craft is only as good as its practitioners. And the better the practitioners, the better the craft. My purpose in life is to improve the craft, one student at a time.</p>
+								<p>My teaching technique is an honest look at the challenges we all face in this rigorous field: persevere, make mistakes, and learn from the difficulties even senior software developers face. It is based on fostering pragmatic, steadfast, and thorough problem solvers - rather than people who simply know how to type code.</p>
+								<blockquote class="blockquote-reverse">
+									<p>"It is the supreme art of the teacher to awaken joy in creative expression and knowledge."</p>
+									<footer>Albert Einstein</footer>
+								</blockquote>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!--coding-->
+				<section>
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12">
+								<h2>Coding</h2>
+								<p>I have been coding for a little over 15 years and, in that time, have learned the importance of learning itself. A good coder learns. A great coder learns even more. This is a rich ecosystem where opportunities to learn, grow, and mess up abound. Great coders adapt and avail themselves of these enormous advantages.</p>
+								<p>With the ubiquity of the web, secure coding has never been more important. The extra time invested in secure coding practices is well worth the effort. Protecting the client's data and interests is worth the upfront investment in ensuring information security, especially as the amount of time required to code securely in the first place has been declining in recent years.</p>
+								<blockquote>
+									<p>"Machines take me by surprise with great frequency."</p>
+									<footer>Alan Turing</footer>
+								</blockquote>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!--advocacy-->
+				<section>
+					<div class="container">
+						<h2>Advocacy</h2>
+						<div class="row row-flex row-flex-wrap">
+							<div class="col-sm-6">
+								<div>
+									<h3>IPv6</h3>
+									<p>I have always been an early adopter and advocate of IPv6. The sooner we all transition from legacy IPv4 to IPv6, the faster and smoother the internet will operate. Excuses to stay with IPv4 are running out faster than IPv4 addresses themselves! IPv6 is easy to deploy and program.</p>
+									<p>I have deployed dual stacked IPv4/IPv6 mail and web services for my main domain, as well as integrated IPv6 awareness in the main curriculum at Deep Dive Coders.</p>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div>
+									<h3>Net Neutrality</h3>
+									<p>The internet was designed as an equal playing field for all involved. Letting other players decide which traffic has priority over which cuts at the very fabric the internet is woven with. A free, transparent, and open internet is the only way to ensure the well being of the internet we have all come to enjoy these past decades.</p>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12">
+								<blockquote class="blockquote-reverse">
+									<p>"I hope we will use the Net to cross barriers and connect cultures."</p>
+									<footer>Tim Berners-Lee</footer>
+								</blockquote>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- ========================== -->
+				<!--  Contact Form Modal        -->
+				<!-- ========================== -->
+				<div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<h4 class="modal-title" id="myModalLabel">Contact Me! :D</h4>
+							</div>
+							<div class="modal-body">
+								<form action="">
+									<div class="form-group">
+										<label for="name">Name</label>
+										<input type="text" class="form-control" id="name" name="name" placeholder="Whom shall I say is calling?">
+									</div>
+									<div class="form-group">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" id="email" name="email" placeholder="How may I reach you?">
+									</div>
+									<div class="form-group">
+										<label for="subject">Subject</label>
+										<input type="text" class="form-control" id="subject" name="subject" placeholder="What's this about?">
+									</div>
+									<div class="form-group">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+									</div>
+									<div class="form-group">
+										<button type="reset" class="btn btn-default">Clear</button>
+										<button type="submit" class="btn btn-primary">Send</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div><!--.sfooter-content-->
 
 		<footer>
-			footer here
+			<div class="container">
+				&copy;&nbsp;footer here
+			</div>
 		</footer>
 	</body>
 </html>
